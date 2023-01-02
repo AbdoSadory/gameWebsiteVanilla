14c375e1f3ec4ec1afbc2bc16d3ce6d1c834004f
Abdelrhman Sayed El-Sadory <abdo_sadory@yahoo.com>
2023-01-02 23:36:40 +0200

    news-section is done

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+// for react https://codesandbox.io/s/030n28?file=/src/App.jsx
+// for core https://codesandbox.io/s/pzq56b?file=/index.html
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: false,
+  },
+});
